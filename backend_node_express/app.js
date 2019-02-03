629256b7b1127d31c7d2733c382b86d6013388e2
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2019-02-03 16:55:01 +0100

    Started API with basic food search

--- backend_node_express/app.js
+++ backend_node_express/app.js
@@ -1,40 +1,74 @@
 const express = require('express')
-const app = express()
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const app = express()
 // Connection URL
 const url = 'mongodb://users:J1ovddMPAbI' + encodeURIComponent('=') + '@ds163054.mlab.com:63054/db-server-side-food';
-// Database Name
+// Database name
 const dbName = 'db-server-side-food';
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Collection name
+const collection_name = 'france';
+// MongoDB client
+var collection, db;
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Food server listening on port 3000!')
   const client = new MongoClient(url);
-  // Use connect method to connect to the server
   client.connect(function (err) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    findDocuments(db, function () {
-      client.close();
-    });
+    db = client.db(dbName);
+    collection = db.collection(collection_name);
   });
 })
 
-const findDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection('vietnam'); // current collection (small one)
-  // Find some documents
-  collection.find({}).toArray(function (err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
+/*
+/* http://localhost:3000/
+*/
+app.get('/', function (req, res) {
+  res.send("Server online");
+})
+/*
+/ * http://localhost:3000/api/stats
+*/
+app.get('/api/stats', function (req, res) {
+  collection.stats(function (err, stats) {
+    res.send({ items: stats.count });
   });
-}
+})
+/*
+/* http://localhost:3000/api/food?page=1&limit=2
+*/
+app.get('/api/foods', function (req, res) {
+  let pagesize = 50;
+  let n = 1;
+  if (req.query.limit != null) {
+    pagesize = parseInt(req.query.limit);
+  }
+  if (req.query.page != null && req.query.page > 0) {
+    n = parseInt(req.query.page);
+  }
+  let searchObject = {};
+  if (req.query.name != null) {
+    var reg = new RegExp(".*" + req.query.name + ".*", "i");
+    searchObject = { "product_name": reg }
+  }
+  collection.find(searchObject).skip(pagesize * (n - 1)).limit(pagesize).toArray(function (err, docs) {
+    assert.equal(err, null);
+    res.send(docs);
+  });
+})
+
+/*
+/* http://localhost:3000/api/food/00000
+*/
+app.get('/api/foods/:itemId', function (req, res) {
+  if (req.params.itemId != null) {
+    collection.find({ _id: req.params.itemId }).toArray(function (err, docs) {
+      assert.equal(err, null);
+      res.send(docs);
+    });
+  } else {
+    res.send({});
+  }
+})
