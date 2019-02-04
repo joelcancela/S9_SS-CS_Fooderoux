f985eca51efc3e1b87aacca54b5161988000a375
pierrebonny <pbonny63@free.fr>
2019-02-04 16:59:43 +0100

    comparison modal ok

--- projet_client_side_vuejs/src/components/ItemsGrid.vue
+++ projet_client_side_vuejs/src/components/ItemsGrid.vue
@@ -22,7 +22,7 @@
                                         <v-label>Chips lays</v-label>
                                     </v-flex>
                                     <v-flex xs3>
-                                        <v-checkbox/>
+                                        <v-checkbox @change="itemSelect" v-model="checkboxes[n]"/>
                                     </v-flex>
                                 </v-layout>
                             </v-card>
@@ -30,7 +30,7 @@
                     </v-layout>
                 </v-flex>
                 <v-flex xs2>
-                    <v-btn>Comparer</v-btn>
+                    <ComparisonModal :buttonDisabled="selectedItems < 2" :items="results" :itemsSelected="selectedItems"/>
                 </v-flex>
             </v-layout>
 
@@ -39,33 +39,64 @@
 </template>
 
 <script>
-    export default {
-        name: "ItemsGrid",
+import ComparisonModal from "./ComparisonModal";
+export default {
+  name: "ItemsGrid",
+  components: {
+    ComparisonModal
+  },
+  data: function() {
+    return {
+      results: [
+        {
+          path:
+            "https://images-na.ssl-images-amazon.com/images/I/910uahYmmPL._SY355AA355_PIbundle-40,TopRight,0,0_AA355_SH20_.jpg",
+          price: 2,
+          name: "chips lays",
+          score: "A"
+        }
+      ],
+      checkboxes: this.initCheckboxes(),
+      selectedItems: 0
+    };
+  },
+  methods: {
+    initCheckboxes() {
+      let array = [];
+      for (let i = 0; i < 9; i++) {
+        array.push(false);
+      }
+      return array;
+    },
+    itemSelect(newValue) {
+      newValue ? this.selectedItems++ : this.selectedItems--;
     }
+  }
+};
 </script>
 
 <style scoped>
-    .card {
-        width: 148px;
-        padding-top: 5px;
-        padding-left: 5px;
-        padding-right: 5px;
-    }
+.card {
+  width: 148px;
+  padding-top: 5px;
+  padding-left: 5px;
+  padding-right: 5px;
+}
 
-    .image {
-        margin-top: 10px;
-        margin-left: 10px;
-        margin-right: 10px;
-    }
+.image {
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
 
-    .itemName {
-        margin-left: 10px;
-    }
-    .mainContainer {
-        padding: 5px;
-    }
-    .list {
-        max-height: 63%;
-        overflow-y: auto;
-    }
+.itemName {
+  margin-left: 10px;
+}
+.mainContainer {
+  padding: 5px;
+}
+.list {
+  max-height: 63%;
+  overflow-y: auto;
+}
 </style>
