5971af24b7b7962355b978979afe876c645f9118
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2019-02-07 11:51:06 +0100

    GEOCODING reverse

--- backend_node_express/app.js
+++ backend_node_express/app.js
@@ -11,6 +11,9 @@ const collection_name = 'france';
 // MongoDB client
 var collection, db;
 
+// http client
+const request = require('request');
+
 app.listen(3000, function () {
   console.log('Food server listening on port 3000!')
   const client = new MongoClient(url);
@@ -20,14 +23,14 @@ app.listen(3000, function () {
     db = client.db(dbName);
     collection = db.collection(collection_name);
   });
-})
+});
 
 /*
 /* http://localhost:3000/
 */
 app.get('/', function (req, res) {
   res.send("Server online");
-})
+});
 /*
 / * http://localhost:3000/api/stats
 */
@@ -35,7 +38,7 @@ app.get('/api/stats', function (req, res) {
   collection.stats(function (err, stats) {
     res.send({ items: stats.count });
   });
-})
+});
 /*
 /* http://localhost:3000/api/food?page=1&limit=2
 */
@@ -57,7 +60,7 @@ app.get('/api/foods', function (req, res) {
     assert.equal(err, null);
     res.send(docs);
   });
-})
+});
 
 /*
 /* http://localhost:3000/api/food/00000
@@ -71,4 +74,56 @@ app.get('/api/foods/:itemId', function (req, res) {
   } else {
     res.send({});
   }
-})
+});
+
+/**
+ * Region reverse geocoding by long/lat
+ * Valid longitude values are between -180 and 180, both inclusive.
+ * Valid latitude values are between -90 and 90 (both inclusive).
+ *
+ * Example: http://localhost:3000/api/regions/resolve?lon=-73.856077&lat=40.848447
+ */
+app.get('/api/regions/resolve', function (req, res) {
+
+  let lon;
+  let lat;
+  if (req.query.lon != null) {
+    lon = parseFloat(req.query.lon);
+    if (lon < -180.0 || lon > 180.0) {
+      res.status(400).send("Valid longitude values are between -180 and 180, both inclusive.");
+      return;
+    }
+  }
+  if (req.query.lat != null) {
+    lat = parseFloat(req.query.lat);
+    if (lat < -90.0 || lat > 90.0) {
+      res.status(400).send("Valid latitude values are between -90 and 90 (both inclusive).");
+      return;
+    }
+  }
+
+  request.get(
+      {
+        url: "https://nominatim.openstreetmap.org/reverse",
+        headers: {
+          'User-Agent': 'NPM/Request S9_WEBSRV',
+          'Referer': 'about:blank'
+        },
+        qs: {
+          "format": "json",
+          "lon": String(lon),
+          "lat": String(lat),
+          "zoom": "10",
+          "addressdetails": "1"
+        },
+        json: true
+      },
+      (e, r, body) => {
+    if (e) {
+      console.log(e);
+      res.status(400).send(e);
+      return;
+    }
+    res.send(body.address);
+  });
+});
