2ea6e0936fd9de118eabe67c20228edcb09e3f00
pierrebonny <pbonny63@free.fr>
2019-02-14 08:48:37 +0100

    fix merge

--- projet_client_side_vuejs/src/views/Home.vue
+++ projet_client_side_vuejs/src/views/Home.vue
@@ -1,55 +1,74 @@
 <template>
-    <v-app>
-        <v-content class="home">
-            <v-layout column>
-                <v-flex xs2>
-                    <v-layout align-center justify-center class="searchBar" row wrap>
-                        <v-flex xs2>
-                            <v-img contain width="50%" :src="coloredIcon"></v-img>
-                        </v-flex>
-                        <v-flex xs10>
-                            <v-text-field label="Solo" :placeholder="placeholder" append-icon="search"
-                                          solo hide-details v-model="search"></v-text-field>
-                        </v-flex>
-                    </v-layout>
+  <v-app>
+    <v-content class="home">
+      <v-layout column>
+        <v-flex xs3>
+          <v-layout align-center justify-center class="searchBar" row wrap>
+            <v-flex xs2>
+              <v-img contain width="50%" :src="coloredIcon"></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-text-field
+                label="Solo"
+                :placeholder="placeholder"
+                append-icon="search"
+                solo
+                hide-details
+                v-model="search"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs2 class="select">
+              <v-menu offset-y>
+                <v-flex class="selectButton" slot="activator">
+                  <flag
+                    class="flagIcon"
+                    :squared="false"
+                    :iso="selectedCountry"
+                  />
+                  <v-icon color="black">arrow_drop_down</v-icon>
                 </v-flex>
+                <v-list>
+                  <v-list-tile
+                    v-for="(country, index) in countries"
+                    :key="index"
+                    @click="toggle(country)"
+                  >
+                    <flag class="flagIcon" :squared="false" :iso="country" />
+                    <v-label>{{ country }}</v-label>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+            </v-flex>
+          </v-layout>
+        </v-flex>
 
-                <v-flex xs8>
-                    <v-layout row>
-                        <v-flex xs2>
-                            <Drawer v-on:map="changeToMap()" v-on:diet="changeToDiet()" v-on:recipe="changeToRecipe()"/>
-                        </v-flex>
-                        <v-flex xs8>
-                            <component :search="search" :filters="filters" v-bind:is="mainComponent"/>
-                        </v-flex>
-                        <v-flex xs2>
-                            <component v-on:update_filters="updateFilters" v-bind:is="filtersComponent"/>
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-                <v-flex xs2 class="select">
-                    <v-menu offset-y>
-                        <v-flex class="selectButton"
-                                slot="activator"
-                        >
-                            <flag class="flagIcon" :squared="false" :iso="selectedCountry"/>
-                            <v-icon color="black">arrow_drop_down</v-icon>
-                        </v-flex>
-                        <v-list>
-                            <v-list-tile
-                                    v-for="(country, index) in countries"
-                                    :key="index"
-                                    @click="toggle(country)"
-                            >
-                                <flag class="flagIcon" :squared="false" :iso="country"/>
-                                <v-label>{{country}}</v-label>
-                            </v-list-tile>
-                        </v-list>
-                    </v-menu>
-                </v-flex>
-            </v-layout>
-        </v-content>
-    </v-app>
+        <v-flex xs9>
+          <v-layout row>
+            <v-flex xs2>
+              <Drawer
+                v-on:map="changeToMap()"
+                v-on:diet="changeToDiet()"
+                v-on:recipe="changeToRecipe()"
+              />
+            </v-flex>
+            <v-flex xs8>
+              <component
+                :search="search"
+                :filters="filters"
+                v-bind:is="mainComponent"
+              />
+            </v-flex>
+            <v-flex xs2>
+              <component
+                v-on:update_filters="updateFilters"
+                v-bind:is="filtersComponent"
+              />
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -71,78 +90,58 @@ export default {
   },
   data: function() {
     return {
-      selectedCountry: "fr",
       coloredIcon: require("../assets/diet_Colored.png"),
       placeholder: "Chercher un aliment",
       mainComponent: "ItemsGrid",
       filtersComponent: "Criterions",
-      countries: ["fr", "us", "jp", "mx", "ch", "ie", "es"]
+      search: "",
+      filters: {},
+      countries: ["fr", "us", "jp", "mx", "ch", "ie", "es"],
+      selectedCountry: "fr"
     };
   },
   methods: {
-      isoFromCountry(country) {
-          switch (country) {
-              case "France":
-                  return "fr";
-              case "Etats-Unis":
-                  return "us";
-              case "Japon":
-                  return "jp";
-              case "Mexique":
-                  return "mx";
-              case "Chine":
-                  return "cn";
-              case "Irlande":
-                  return "ie";
-              case "Espagne":
-                  return "es";
-              default:
-                  return;
-          }
-      },
-      toggle(country) {
-          this.selectedCountry === "fr" ? this.selectedCountry = country : this.selectedCountry = "fr";
-      },
-      changeToDiet() {
-          this.coloredIcon = require("../assets/diet_Colored.png");
-          this.placeholder = "Chercher un aliment";
-          this.mainComponent = "ItemsGrid";
-          this.filtersComponent = "Criterions";
-      },
-      data: function () {
-          return {
-              coloredIcon: require("../assets/diet_Colored.png"),
-              placeholder: "Chercher un aliment",
-              mainComponent: "ItemsGrid",
-              filtersComponent: "Criterions",
-              search: "",
-              filters: {}
-          };
-      },
-      methods: {
-          changeToDiet() {
-              this.coloredIcon = require("../assets/diet_Colored.png");
-              this.placeholder = "Chercher un aliment";
-              this.mainComponent = "ItemsGrid";
-              this.filtersComponent = "Criterions";
-          },
-          changeToRecipe() {
-              this.coloredIcon = require("../assets/recipe-book_Colored.png");
-              this.placeholder = "Chercher une recette";
-              this.mainComponent = "RecipeView";
-              this.filtersComponent = "Criterions";
-          },
-          changeToMap() {
-              this.coloredIcon = require("../assets/store_Colored.png");
-              this.placeholder = "Chercher un magasin";
-              this.mainComponent = "MapView";
-              this.filtersComponent = "RecipeFilters";
-          },
-          updateFilters(filters) {
-              this.filters = filters;
-              console.log("FILTERS", filters);
-          }
+    isoFromCountry(country) {
+      switch (country) {
+        case "France":
+          return "fr";
+        case "Etats-Unis":
+          return "us";
+        case "Japon":
+          return "jp";
+        case "Mexique":
+          return "mx";
+        case "Chine":
+          return "cn";
+        case "Irlande":
+          return "ie";
+        case "Espagne":
+          return "es";
+        default:
+          return;
       }
+    },
+    changeToDiet() {
+      this.coloredIcon = require("../assets/diet_Colored.png");
+      this.placeholder = "Chercher un aliment";
+      this.mainComponent = "ItemsGrid";
+      this.filtersComponent = "Criterions";
+    },
+    changeToRecipe() {
+      this.coloredIcon = require("../assets/recipe-book_Colored.png");
+      this.placeholder = "Chercher une recette";
+      this.mainComponent = "RecipeView";
+      this.filtersComponent = "Criterions";
+    },
+    changeToMap() {
+      this.coloredIcon = require("../assets/store_Colored.png");
+      this.placeholder = "Chercher un magasin";
+      this.mainComponent = "MapView";
+      this.filtersComponent = "RecipeFilters";
+    },
+    updateFilters(filters) {
+      this.filters = filters;
+    }
   }
 };
 </script>
@@ -168,17 +167,17 @@ export default {
   height: 16px;
 }
 .selectButton {
-    display: flex;
-    flex-direction: row;
-    background-color: white !important;
-    border-radius: 2px;
-    align-items: center;
-    justify-content: center;
-    padding-left: 5px;
+  display: flex;
+  flex-direction: row;
+  background-color: white !important;
+  border-radius: 2px;
+  align-items: center;
+  justify-content: center;
+  padding-left: 5px;
 }
 .select {
-    display: flex;
-    align-items: flex-end;
-    justify-content: flex-end;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
 }
 </style>
