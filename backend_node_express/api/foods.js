2c1ebd833823ffc9cf2f8e56801c24cb352f0d66
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2019-02-21 11:30:25 +0100

    Fixing ingredients filter

--- backend_node_express/api/foods.js
+++ backend_node_express/api/foods.js
@@ -101,7 +101,10 @@ function getFoods(req, res, next) {
             criterias.push({ $or: [{ nutrition_grade_fr: { $in: grade_array }, nutrition_grades: { $in: grade_array } }] });
         }
     } if (req.query.ingredients != null) {
-        criterias.push({ $or: [{ ingredients_text_fr: reg, ingredients_text: reg, ingredients_tags: regArray, ingredients: regArray }] });
+        let reg = new RegExp(".*" + req.query.ingredients + ".*", "i");
+        let regIngredientsArray = regArray;
+        regIngredientsArray.$elemMatch = reg;
+        criterias.push({ $or: [{ ingredients_text_fr: reg, ingredients_text: reg, ingredients_tags: regIngredientsArray, ingredients: regIngredientsArray }] });
     } if (req.query.additives != null) {
         let criteria_additives = (req.query.additives == "" ? {$in: [null, []]} : { '$regex': '^((?!' + req.query.additives + ').)*$', '$options': 'i' });
         criterias.push({ additives: criteria_additives });
@@ -110,7 +113,10 @@ function getFoods(req, res, next) {
     } if (req.query.allergens != null) {
         criterias.push({ $or: [{ allergens_tags: reg, allergens: reg, allergens_from_ingredients: reg, traces: reg }] });
     } if (req.query.vitamins != null) {
-        criterias.push({ vitamins_tags: regArray });
+        let reg = new RegExp(".*" + req.query.vitamins + ".*", "i");
+        let regVitaminsArray = regArray;
+        regVitaminsArray.$elemMatch = reg;
+        criterias.push({ vitamins_tags: regVitaminsArray });
     }
     let searchObject = {};
     if (criterias.length > 0) {
